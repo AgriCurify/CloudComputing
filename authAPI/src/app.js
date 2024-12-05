c924b88bd54418eb55fae8c8608e645b417bb81c
Xechi <msyuaib776@gmail.com>
2024-12-05 18:53:43 +0800

    Update logs

--- authAPI/src/app.js
+++ authAPI/src/app.js
@@ -1,15 +1,14 @@
 const express = require('express');
-const logRequest = require('./middleware/logs');
 const authRoute = require('./routes/authRoute');
 const userRoute = require('./routes/userRoute');
 
 const app = express();
 
 app.use(express.json());
-app.use(logRequest);
 
-app.get("/", (req, res) => {
-  res.send("API AgriCurify");
+app.use((req, res, next) => {
+  console.log("Logs occurred request to:", req.method, req.path);
+  next();
 });
 
 app.use(authRoute);
--- authAPI/src/middleware/logs.js
+++
@@ -1,6 +0,0 @@
-const logRequest = (req, res, next) => {
-  console.log("logs occurred request to:", req.method, req.path);
-  next();
-};
-
-module.exports = logRequest;
