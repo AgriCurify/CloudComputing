79a618e2278d10a50128f7ffa23eb0db1fb933fb
Xechi <msyuaib776@gmail.com>
2024-12-06 15:16:38 +0800

    Add change photo profile

--- authAPI/src/routes/userRoute.js
+++ authAPI/src/routes/userRoute.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const auth = require('../middleware/authentication')
-const { getByTokenUser, changePassword, updateUser } = require('../controller/userController');
+const { getByTokenUser, updateUser, updateProfileImage, changePassword } = require('../controller/userController');
 
 const router = express.Router();
 
 router.get("/users", auth, getByTokenUser);
 router.put("/users", auth, updateUser);
+router.put("/users/updateProfile", auth, updateProfileImage);
 router.put("/users/changePassword", auth, changePassword);
 
 
